3e54cfe696f53a8610831c22fd2b26ea14acfdb1
JerichoLirio <jericho_lirio@dlsu.edu.ph>
2024-08-02 17:24:48 +0800

    test

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config();  // Load environment variables from .env file
 
-mongoose.connect('mongodb://localhost:27017/data', {});
+// Use the MONGO_URI from the environment variables
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -27,11 +36,8 @@ const commentSchema = new mongoose.Schema({
     postID: {type: Number}
 });
 
-
-
-
 const User = mongoose.model('User', userSchema);
 const Post = mongoose.model('Post', postSchema);
 const Comment = mongoose.model('Comment', commentSchema);
 
-module.exports = { User, Post, Comment};
+module.exports = { User, Post, Comment };
